1605fd557dccf270765c21d5faf87b0105b8f22a
KeiserDallas3205 <62128647+KeiserDallas3205@users.noreply.github.com>
2022-10-10 09:01:03 -0500

    Update clientTCPExample.py

--- clientTCPExample.py
+++ clientTCPExample.py
@@ -1,16 +1,28 @@
+"""
+Name: Keiser Dallas
+Date: 10/10/2022
+Desc: This program is an example of establishing TCP connection on the client side.
+      The program prompts the user for a server ip address and port number, then it
+      sends the desired message to that server. Afterwards, an altered message is
+      received from the server and the connection is closed. 
+
+"""
+
+# Import socket for establishing TCP connection
 from socket import *
 
+# Create the client socket
 client_socket = socket(AF_INET, SOCK_STREAM)
 
-# Create a client socket w/ given IP and port number
+# Ask user for server IP address and port number
 server_ip = input("Server ip: ")
 server_port = input("Server port: ")
 
-# Connect socket to the server
+# Connect client socket to the server socket
 client_socket.connect((server_ip, int(server_port)))
 print("Connection established.\n")
 
-# Ask for a message and send it to server socket 
+# Ask user for a message and send it to server socket 
 message = input("Message: ")
 client_socket.send(message.encode())
 
